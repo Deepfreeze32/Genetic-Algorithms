86f8691c8ba491d01dbf3b184ba893ba096ab350
Deepfreeze32 <tcc10a@acu.edu>
2013-04-22 19:38:28 -0500

    Initial Commit.

---
+++ src/nqueens/NQueens.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nqueens;
+
+/**
+ * The N-Queens Satisfiability solving problem.
+ * @author tcc10a
+ */
+public class NQueens {
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
---
+++ src/satsolving/SatSolving.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package satsolving;
+
+/**
+ * The Satisfiability Solving GA
+ * @author tcc10a
+ */
+public class SatSolving {
+    public static void main(String[] args) {
+        
+    }
+}
